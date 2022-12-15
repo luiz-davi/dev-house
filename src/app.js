ffc31893c1fc85fcf20e5b1224bd6b5644f41b31
luiz-davi <luizd4398@gmail.com>
2022-12-15 20:37:44 -0300

    configurando mongodb

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import routes from './routes';
-
+import mongoose from 'mongoose';
 
 class App {
   constructor(){
     this.server = express();
+    
+    mongoose.set("strictQuery", true);
+    mongoose.connect('mongodb+srv://ravengar:ravengar@cluster0.5bagg4g.mongodb.net/devhouse?retryWrites=true&w=majority', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    mongoose.set('strictQuery', true);
 
     this.middlewares();
     this.routes();
