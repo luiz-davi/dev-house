8e5efc53b376b56cde973c636e69eee6a23c0d10
luiz-davi <luizd4398@gmail.com>
2022-12-17 13:38:35 -0300

    Crud de casas

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,12 @@ const routes = new Router();
 const upload = multer(uploadConfig);
 
 routes.post('/sessions', SessionsController.store);
+
+routes.get('/casas', CasasController.index);
+routes.get('/casas/:id', CasasController.show);
 routes.post('/casas', upload.single('capa'), CasasController.store);
+routes.put('/casas/:id', upload.single('capa'), CasasController.update);
+routes.delete('/casas/:id', CasasController.destroy);
+
 
 module.exports = routes;
